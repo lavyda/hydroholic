a02710de830d5757abf6c531a0ff58a5cc7b997f
Vladimír Kovalský <kvl.vlado@gmail.com>
2022-12-27 12:20:35 +0100

    get feature for date

--- src/routes/features/[feature]/+page.server.ts
+++ src/routes/features/[feature]/+page.server.ts
@@ -1,12 +1,20 @@
 import { error } from '@sveltejs/kit';
+import { features } from '../../../common/services/features';
 import { getBlocks } from '../../../cityPoolKosice/cityPoolKosice';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, url }) => {
+  const featureId = Number(params.feature);
+  const feature = features.find((f) => f.id === featureId);
+  const date = url.searchParams.get('date') ?? undefined;
+  if (!feature) {
+    throw error(404);
+  }
+
   try {
-    const blocks = await getBlocks(Number(params.feature));
-    return { blocks };
+    const blocks = await getBlocks(featureId, date);
+    return { ...feature, blocks };
   } catch {
-    throw error(500, 'Error');
+    throw error(500);
   }
 }) satisfies PageServerLoad;
