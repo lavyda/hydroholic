377fe6b11a01d164fdeaf3496218806de18f90bd
Vladimír Kovalský <kvl.vlado@gmail.com>
2022-12-29 16:45:00 +0100

    activate pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,42 @@
 <script>
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
 	import '../app.postcss';
+
+	let ReloadPrompt;
+	onMount(async () => {
+		if (pwaInfo) {
+			ReloadPrompt = (await import('$lib/ReloadPrompt.svelte')).default;
+
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					// r && setInterval(() => {
+					//    console.log('Checking for sw update')
+					//    r.update()
+					// }, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
-<slot />
+<svelte:head>
+	{@html webManifest}
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+{#if ReloadPrompt}
+	<svelte:component this={ReloadPrompt} />
+{/if}
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,34 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), SvelteKitPWA({ registerType: 'autoUpdate' })],
+	plugins: [sveltekit(), SvelteKitPWA({
+		registerType: 'autoUpdate',
+		includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
+		manifest: {
+			name: 'Hydroholic',
+			short_name: 'Hydroholic',
+			description: 'Information for various water sports',
+			theme_color: '#ffffff',
+			icons: [
+				{
+					src: 'android-chrome-192x192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				{
+					src: 'android-chrome-512x512.png',
+					sizes: '512x512',
+					type: 'image/png'
+				},
+				{
+					src: 'android-chrome-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any maskable'
+				}
+			]
+		},
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
